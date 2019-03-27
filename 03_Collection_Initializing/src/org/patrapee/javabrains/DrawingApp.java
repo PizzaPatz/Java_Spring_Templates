2cf128184f57a768e219fab0c1843dcf038da603
PizzaPatz <patpongtana@gmail.com>
2019-03-27 11:04:17 -0700

    added Collection_Initializing

---
+++ 03_Collection_Initializing/src/org/patrapee/javabrains/DrawingApp.java
@@ -0,0 +1,15 @@
+package org.patrapee.javabrains;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+    public static void main(String[] args) {
+//        ClassPathXmlApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        Triangle triangle = (Triangle) context.getBean("triangle-alias");
+        triangle.draw();
+    }
+}
+
